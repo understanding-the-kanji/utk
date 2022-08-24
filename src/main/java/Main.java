32cb40a41a7292cbc4dd169f23e9e50e773f16cc
Samuel Mace <samuelcmace@gmail.com>
2022-08-24 15:59:30 -0500

    Add Drag and Drop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,11 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.Scene;
 import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
@@ -20,17 +19,24 @@ public class Main extends Application
 
     private Scene scene;
     private Group group;
-    private Canvas canvas;
-    private GraphicsContext canvasContext;
     private Logger logger;
+
+    private Rectangle rect;
+
+    private double clickX;
+    private double clickY;
 
     public Main()
     {
         this.group = new Group();
         this.scene = new Scene(this.group, windowWidth, windowHeight);
-        this.canvas = new Canvas(windowWidth, windowHeight);
 
         this.logger = new Logger("./log.txt");
+
+        this.rect = new Rectangle();
+
+        this.clickX = 0.0d;
+        this.clickY = 0.0d;
     }
 
     public static void main(String args[]) { launch(args); }
@@ -39,27 +45,53 @@ public class Main extends Application
     {
         primaryStage.setTitle(windowTitle);
 
-        this.canvasContext = this.canvas.getGraphicsContext2D();
+        this.rect.setStrokeWidth(2);
+        this.rect.setFill(Color.RED);
+        this.rect.setHeight(200);
+        this.rect.setWidth(200);
+        this.rect.setX(100);
+        this.rect.setY(100);
+        this.rect.toFront();
 
-        this.canvasContext.setFill(Color.RED);
-        this.canvasContext.fillRect(100, 100, 200, 200);
+        this.group.getChildren().add(this.rect);
 
-        this.canvas.setOnMousePressed(new EventHandler<MouseEvent>() {
+        this.group.setOnMousePressed(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                logger.ConsoleInformation("The mouse is being clicked!");
+                try {
+                    System.out.println("Clicked Coordinates: " + event.getX() + ", " + event.getY());
+                    clickX = event.getX();
+                    clickY = event.getY();
+                }
+                catch(Exception ex)
+                {
+                    System.out.println("There was an error in getting the origin coordinates of the Rectangle object.");
+                }
             }
         });
 
-        this.canvas.setOnMouseDragged(new EventHandler<MouseEvent>() {
+        this.group.setOnMouseDragged(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                logger.ConsoleInformation("The mouse is being dragged!");
+                try {
+                    System.out.println("Set Coordinates: " + event.getX() + ", " + event.getY());
+
+                    clickX = event.getX();
+                    clickY = event.getY();
+
+                    Rectangle testRect = (Rectangle) event.getTarget();
+
+                    testRect.setX(clickX);
+                    testRect.setY(clickY);
+                }
+                catch(Exception ex)
+                {
+                    System.out.println("There was an error in setting the Rectangle's new coordinates.");
+                }
             }
         });
 
-        this.group.getChildren().add(this.canvas);
-
+        primaryStage.setResizable(false);
         primaryStage.setScene(this.scene);
         primaryStage.show();
     }
