91900479b60d88a26a980e6c87fad5bea0869e9b
Samuel Mace <samuelcmace@gmail.com>
2022-09-14 15:31:41 -0500

    Add LoadScene with Controls

--- src/main/java/net/samuelcmace/utk/Main.java
+++ src/main/java/net/samuelcmace/utk/Main.java
@@ -39,7 +39,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         stage.setTitle("Hello!");
-        stage.setScene(this.quizScene);
+        stage.setScene(this.loadScene);
         stage.show();
     }
 
