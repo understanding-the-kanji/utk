4f3784b4680bf6464d690f85072f969766083454
Samuel Mace <samuelcmace@gmail.com>
2022-09-14 11:29:34 -0500

    Remove Duplicate Main Class

--- src/main/java/net/samuelcmace/utk/Main.java
+++ src/main/java/net/samuelcmace/utk/Main.java
@@ -1,57 +1,35 @@
 package net.samuelcmace.utk;
 
-import net.samuelcmace.utk.gui.scene.EditorScene;
-import net.samuelcmace.utk.gui.scene.QuizScene;
-
 import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.scene.Group;
-import javafx.scene.input.MouseEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import net.samuelcmace.utk.gui.scene.LoadScene;
-
 import net.samuelcmace.utk.util.Logger;
 import net.samuelcmace.utk.util.Scheduler;
 
-public class Main extends Application
-{
+import java.io.IOException;
+
+public class Main extends Application {
+
     private Logger logger;
     private Scheduler scheduler;
-
-    private Group loadSceneGroup;
-    private Group quizSceneGroup;
-    private Group editorSceneGroup;
-
-    private LoadScene loadScene;
-    private QuizScene quizScene;
-    private EditorScene editorScene;
 
     public Main()
     {
         this.logger = new Logger();
         this.scheduler = new Scheduler(this.logger);
-
-        this.loadSceneGroup = new Group();
-        this.quizSceneGroup = new Group();
-        this.editorSceneGroup = new Group();
-
-        this.loadScene = new LoadScene(this.loadSceneGroup, this.logger.ApplicationData.WindowWidth, this.logger.ApplicationData.WindowHeight, this.logger);
-        this.quizScene = new QuizScene(this.quizSceneGroup, this.logger.ApplicationData.WindowWidth, this.logger.ApplicationData.WindowHeight, this.logger);
-        this.editorScene = new EditorScene(this.editorSceneGroup, this.logger.ApplicationData.WindowWidth, this.logger.ApplicationData.WindowHeight, this.logger);
     }
 
-    public static void main(String args[]) { launch(args); }
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
 
-    @Override public void start(Stage primaryStage) throws Exception
-    {
-        primaryStage.setTitle(this.logger.ApplicationData.WindowName);
-        primaryStage.setResizable(false);
-        primaryStage.setScene(this.loadScene);
-
-        this.loadScene.QuizSceneButton.setOnMouseClicked(new EventHandler<MouseEvent>() { @Override public void handle(MouseEvent event) { primaryStage.setScene(quizScene); }});
-        this.loadScene.EditorSceneButton.setOnMouseClicked(new EventHandler<MouseEvent>() { @Override public void handle(MouseEvent event) { primaryStage.setScene(editorScene); }});
-
-        primaryStage.show();
+    public static void main(String[] args) {
+        launch();
     }
 }
--- src/main/java/net/samuelcmace/utk/gui/scene/LoadScene.java
+++ src/main/java/net/samuelcmace/utk/gui/scene/LoadScene.java
@@ -2,12 +2,6 @@ package net.samuelcmace.utk.gui.scene;
 
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.Group;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 
 import net.samuelcmace.utk.util.Logger;
 
@@ -15,41 +9,9 @@ public class LoadScene extends Scene {
 
     private Logger logger;
 
-    private Group mainGroup;
-
-    private Text titleText;
-
-    private VBox mainLayout;
-
-    private HBox quizSceneHBox;
-    private Label quizSceneLabel;
-    public Button QuizSceneButton;
-
-    private HBox editorSceneHBox;
-    private Label editorSceneButton;
-    public Button EditorSceneButton;
-
     public LoadScene(Parent parent, double width, double height, Logger m_logger)
     {
         super(parent, width, height);
         this.logger = m_logger;
-
-        this.mainGroup = new Group();
-
-        this.mainLayout = new VBox();
-        this.titleText = new Text("Welcome to the LoadScene!");
-        this.mainLayout.getChildren().add(titleText);
-
-        this.quizSceneHBox = new HBox();
-        this.QuizSceneButton = new Button("Quiz Myself");
-        this.quizSceneHBox.getChildren().add(this.QuizSceneButton);
-        this.mainGroup.getChildren().add(this.quizSceneHBox);
-
-        this.editorSceneHBox = new HBox();
-        this.EditorSceneButton = new Button("Kanji Builder");
-        this.editorSceneHBox.getChildren().add(this.EditorSceneButton);
-        this.mainGroup.getChildren().add(this.editorSceneHBox);
-
-        this.mainGroup.getChildren().add(this.mainLayout);
     }
 }
