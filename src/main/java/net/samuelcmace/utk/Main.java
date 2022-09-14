2a3f05a4d7fdae798f4064fbb82052c0caca1075
Samuel Mace <samuelcmace@gmail.com>
2022-09-14 11:30:53 -0500

    Remove hello-view.fxml and Add Contents to LoadScene.fxml

--- src/main/java/net/samuelcmace/utk/Main.java
+++ src/main/java/net/samuelcmace/utk/Main.java
@@ -22,7 +22,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LoadScene.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
